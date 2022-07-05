cd5b8aa321317608982e758ba8254ebbe3dbe4ac
LuisChan0805 <oleksii.kovalenko1@gmail.com>
2022-07-04 23:51:38 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -22,53 +22,15 @@ const store = configureStore();
 
 function App() {
   return (
-    <Provider store={store}>
-         <MuiThemeProvider>
-                <AppRouter />
-         </MuiThemeProvider>
-    </Provider>
+    <React.Fragment>
+      <Provider store={store}>
+           <MuiThemeProvider>
+                  <AppRouter />
+           </MuiThemeProvider>
+      </Provider>
+    </React.Fragment>
+
   );
 }
 
 export default App;
-
-// const product = {
-//     productName: "Product Name",
-//     productImage: imageWatch,
-//     sellerName: "Seller Name",
-//     quantity: 1,
-//     price: 19.99,
-//     productID: 1
-// };
-
-// // initial load, check if user is logged in
-// const access_token = window.localStorage.getItem(ACCESS_TOKEN);
-// const headers = {Accept: "application/json", Authorization: `Bearer ${access_token}`};
-// axios.get(getUserCartAPI, {headers})
-//     .then((response) => {
-//         store.dispatch(loginUser());
-//         response.data.map((item) => {
-//             const productName = item.name;
-//             const productImage = item.image;
-//             const sellerName = item.sellerName;
-//             const ratings = item.ratings;
-//             const quantity = 1;
-//             const price = item.price;
-//             const productID = item.productId;
-//             const product = {
-//                 productName,
-//                 productImage,
-//                 sellerName,
-//                 ratings,
-//                 quantity,
-//                 price,
-//                 productID
-//             };
-//             store.dispatch(addToCartHelper(product));
-//         })
-//     })
-//     .catch((error) => {
-//         window.localStorage.removeItem(ACCESS_TOKEN);
-//         store.dispatch(logoutUser());
-//     });
-
